88cd5db77ab3ad337868ad3a4d131508448422fa
Maksasj <radioboos@gmail.com>
2022-07-15 22:08:55 +0300

    GameObject generic

---
+++ src/main/java/org/laplace/systems/objectsystem/GameObject.java
@@ -0,0 +1,21 @@
+package org.laplace.systems.objectsystem;
+
+public class GameObject {
+    private String name;
+
+    public GameObject(String name) {
+        this.name = name;
+    }
+
+    public void Update() {
+
+    }
+
+    public void Draw() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
