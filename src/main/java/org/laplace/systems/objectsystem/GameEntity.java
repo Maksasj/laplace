9e94ad85bbbcf681697d3e13c586eb88df2b9c4a
Maksasj <radioboos@gmail.com>
2022-07-15 23:31:20 +0300

    Test GameEntity object

--- src/main/java/org/laplace/systems/objectsystem/GameEntity.java
+++ src/main/java/org/laplace/systems/objectsystem/GameEntity.java
@@ -1,6 +1,10 @@
 package org.laplace.systems.objectsystem;
 
+import com.raylib.Jaylib;
+import org.laplace.Game;
+
 public class GameEntity extends GameObject{
+    private Jaylib.Vector3 pos;
 
     public GameEntity(String name) {
         super(name);
@@ -13,6 +17,10 @@ public class GameEntity extends GameObject{
 
     @Override
     public void Draw() {
-        model.draw()
+        Game.getModelManager().DrawModel(this.getName(), pos);
+    }
+
+    public void setPos(Jaylib.Vector3 pos) {
+        this.pos = pos;
     }
 }
---
+++ src/main/java/org/laplace/systems/objectsystem/GameEntitys/Goblin.java
@@ -0,0 +1,10 @@
+package org.laplace.systems.objectsystem.GameEntitys;
+
+import org.laplace.systems.objectsystem.GameEntity;
+
+public class Goblin extends GameEntity {
+
+    public Goblin() {
+        super("goblin");
+    }
+}
