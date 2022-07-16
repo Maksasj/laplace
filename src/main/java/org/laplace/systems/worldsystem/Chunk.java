7dfe4bc0eaf7e3dc48912c0d55fd62b08b441678
ddf0 <blackman20041996@gmail.com>
2022-07-16 18:52:43 +0300

    fix
    fix

--- src/main/java/org/laplace/systems/worldsystem/Chunk.java
+++ src/main/java/org/laplace/systems/worldsystem/Chunk.java
@@ -69,6 +69,9 @@ public class Chunk extends ChunkMesh{
         }
     }
 
+    public void setEntity(GameEntity entity, int x, int y) {
+        gameObjects[x][y] = entity;
+    }
     public void changeType(ChunkTypes type){
         chankType = type;
     }
